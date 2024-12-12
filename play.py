205bfb2266f4c57c8e3f26b8414cce991b9dc4a3
HBilel101 <hbilel992@gmail.com>
2024-12-12 20:19:50 +0100

    added argparse

--- play.py
+++ play.py
@@ -1,4 +1,7 @@
 from nim import train, play
 import argparse
-ai = train(30000)
+parser = argparse.ArgumentParser(prog="NIM AI Game",description="play with NIM AI agent")
+parser.add_argument("-n",type=int,help='defines the number of epochs the model trains',default=10000)
+args = parser.parse_args()
+ai = train(args.n)
 play(ai)
